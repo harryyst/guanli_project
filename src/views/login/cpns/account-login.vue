ecb48746164bd42294c4b594e47a64dce89b8a7d
harryyst <873634000@qq.com>
2022-04-01 20:14:18 +1000

    login page

--- src/views/login/cpns/account-login.vue
+++ src/views/login/cpns/account-login.vue
@@ -1,13 +1,40 @@
 <template>
-  <div></div>
+  <div class="account-login">
+    <el-form :rules="rules" :model="account" ref="formRef">
+      <el-form-item label="账号" prop="name">
+        <el-input v-model="account.name" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password" showpassword>
+        <el-input v-model="account.password" />
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, reactive, ref } from 'vue';
+import type { FormInstance, ElForm } from 'element-plus';
+import { rules } from '../config/account-config';
 export default defineComponent({
   setup() {
-    return {};
+    const formRef = ref<InstanceType<typeof ElForm>>();
+    const account = reactive({
+      name: '',
+      password: ''
+    });
+    const loginAction = (iskeepPassword: boolean) => {
+      formRef.value?.validate((valid) => {
+        if (valid) {
+          console.log('login');
+        }
+      });
+    };
+    return {
+      account,
+      rules,
+      loginAction,
+      formRef
+    };
   }
 });
 </script>
--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -8,7 +8,7 @@
             <span>账号登录</span>
           </span>
         </template>
-        <account-login></account-login>
+        <account-login ref="accountRef"></account-login>
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
@@ -20,12 +20,20 @@
         <phone-login></phone-login>
       </el-tab-pane>
     </el-tabs>
+    <div class="account-control">
+      <el-checkbox v-model="iskeepPassword">记住密码</el-checkbox>
+      <el-link type="primary">忘记密码</el-link>
+    </div>
+
+    <el-button type="primary" class="login-btn" @click="handleLogin"
+      >立即登陆</el-button
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { Calendar } from '@element-plus/icons-vue';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import AccountLogin from './account-login.vue';
 import PhoneLogin from './phone-login.vue';
 export default defineComponent({
@@ -35,7 +43,17 @@ export default defineComponent({
     PhoneLogin
   },
   setup() {
-    return {};
+    const iskeepPassword = ref(false);
+    const accountRef = ref<InstanceType<typeof AccountLogin>>();
+    const handleLogin = () => {
+      // console.log(accountRef.value);
+      accountRef.value?.loginAction(iskeepPassword.value);
+    };
+    return {
+      iskeepPassword,
+      handleLogin,
+      accountRef
+    };
   }
 });
 </script>
@@ -47,5 +65,14 @@ export default defineComponent({
   .title {
     text-align: center;
   }
+}
+.account-control {
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-between;
+}
+.login-btn {
+  width: 100%;
+  margin-top: 10px;
 }
 </style>
