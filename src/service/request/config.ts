2864ee8f81ccc0051e57e783af7cee62c594249b
harryyst <873634000@qq.com>
2022-04-15 19:45:46 +1000

    ss

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -5,10 +5,10 @@ if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://152.136.185.210:5000/';
   TIME_OUT = 10000;
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = '';
+  BASE_URL = 'http://152.136.185.210:5000/';
   TIME_OUT = 10000;
 } else {
-  BASE_URL = '';
+  BASE_URL = 'http://152.136.185.210:5000/';
   TIME_OUT = 10000;
 }
 
